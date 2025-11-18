50e3fbdee2fa50cf581f54180bf89d46cc84af02
vishal9894 <vishalkumar662002@gmail.com>
2025-11-18 20:59:28 +0530

    add cors

--- server.js
+++ server.js
@@ -4,11 +4,20 @@ const userRoutes = require("./routes/userRoute");
 const cookiesParser = require("cookie-parser")
 const app = express();
 const PORT = 3000;
+const cors = require("cors")
 
 const server = async () => {
   try {
     app.use(express.json());
     app.use(cookiesParser());
+    
+    
+    app.use(cors({
+      origin: "http://localhost:5173", 
+      credentials: true, 
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      allowedHeaders: ["Content-Type", "Authorization"]
+    }));
 
     await connectDB();
 
